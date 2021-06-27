d2371192080ff3e0ad7075bae146ac7cc1d6c38e
SmithRBG <smit36000@gmail.com>
2021-06-27 18:06:49 -0400

    update

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if('MIT License')
-  then("License: MIT")
-    else('none')
-}
+function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
