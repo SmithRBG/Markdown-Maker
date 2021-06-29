42260d53479309ee999eebdfc869e7ed20697002
SmithRBG <smit36000@gmail.com>
2021-06-28 20:20:15 -0400

    Function Update

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,18 @@
 // If there is no license, return an empty string
 const Mit = 'https://img.shields.io/badge/License-MIT-yellow.svg';
 
+const licenseImage = {
+  Img0: 'https://img.shields.io/badge/License-MIT-yellow.svg',
+  Img1: 'https://img.shields.io/badge/License-LGPL%20v3-blue.svg',
+  Img2: 'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg',
+  Img3: 'https://img.shields.io/badge/License-AGPL%20v3-blue.svg',
+  Img4: 'https://img.shields.io/badge/license-Unlicense-blue.svg',
+  Img5: 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+  Img6: 'https://img.shields.io/badge/License-GPLv3-blue.svg',
+  Img7: '',
+};
+
+
 const licenseList = {
   zero:'MIT License',
   one:'GNU Lesser General Public License v3.0',
@@ -9,14 +21,14 @@ const licenseList = {
   three:'GNU Affero General Public License v3.0',
   four:'The Unlicense',
   five:'Apache License 2.0',
-  six:'GNU General Public License v3.0'
+  six:'GNU General Public License v3.0',
 };
 
 function renderLicenseBadge(license) {
-  if (license === 'MIT License'){
+  if (license === licenseList.zero || licenseList.one || licenseList.two || licenseList.three || licenseList.four || licenseList.five || licenseList.six){
     return Mit
   }
-  else {license != 'MIT License'}{
+  else {license != licenseList[7]}{
     return ('')
   };
 };
@@ -24,10 +36,10 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  const photo = 'https://opensource.org/licenses/MIT';
+  const licenseLink = ("https://opensource.org/licenses/MIT");
 
   if (license === 'MIT License'){
-    return photo
+    return licenseLink
   }
   else {licese != 'MIT License'}{
     return ('')
